5d621b34862d9df319db027b038a8a5ce9f09042
gab1144 <gabry.rinciari04@gmail.com>
2023-01-23 16:32:46 +0100

    [add] type, one to many

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -18,6 +18,19 @@
         </div>
 
         <div class="mb-3">
+            <label for="type_id" class="form-label">Tipo</label>
+            <select class="form-select" name="type_id" aria-label="Default select example">
+                <option value="">Selezionare un tipo</option>
+                @foreach ($types as $type)
+                    <option
+                    @if($type->id == old('type_id')) selected @endif
+                     value="{{$type->id}}">{{$type->name}}</option>
+                @endforeach
+            </select>
+
+        </div>
+
+        <div class="mb-3">
           <label for="client_name" class="form-label">Nome cliente</label>
           <input type="text" class="form-control @error('client_name') is-invalid @enderror" name="client_name" id="client_name" value="{{old('client_name', $project->client_name)}}">
           @error('client_name')
